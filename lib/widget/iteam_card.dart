9cefed2f80eca6adfb26faadca2b0ce0b01f2384
SaraHussein80 <93788105+SaraHussein80@users.noreply.github.com>
2024-02-03 06:59:38 +0200

    Add files via upload

---
+++ lib/widget/iteam_card.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:login/screens/news_details_page.dart';
+
+import 'package:login/widget/custom_carousel_slider.dart';
+
+import 'package:login/widget/news_item.dart';
+import 'package:login/widget/recommendation_item.dart';
+import 'package:login/widget/title_home.dart';
+
+class IteamCard extends StatefulWidget {
+  const IteamCard({super.key});
+
+  @override
+  State<IteamCard> createState() => _IteamCardState();
+}
+
+class _IteamCardState extends State<IteamCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 16.0,
+        ),
+        TitleHome(
+          title: 'Breaking News',
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        const CustemCarouselSlider(),
+        const SizedBox(
+          height: 16.0,
+        ),
+        TitleHome(
+          title: 'Recommendation',
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        ...news
+            .map((newsItem) => Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0, left: 10.0),
+                  child: InkWell(
+                      onTap: () {
+                        final index = news.indexOf(newsItem);
+                        Navigator.of(context, rootNavigator: true)
+                            .push(
+                              CupertinoPageRoute(
+                                builder: (_) => NewsDetailsPage(index: index),
+                              ),
+                            )
+                            .then((value) => setState(
+                                  () {},
+                                ));
+                      },
+                      child: RecommendationItem(newsItem: newsItem)),
+                ))
+            .toList(),
+      ],
+    );
+  }
+}
